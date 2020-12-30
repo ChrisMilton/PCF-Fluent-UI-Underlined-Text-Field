88a57067f9c6fc61568fff66c03b8485f5fd9d92
Chris Milton <chris.milton@evorio.co.uk>
2020-12-30 12:40:56 +0000

    Added live dfata output

--- UnderlinedTextField/UnderlinedTextField.tsx
+++ UnderlinedTextField/UnderlinedTextField.tsx
@@ -1,23 +1,32 @@
 import * as React from 'react';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
 
-interface IUnderLinedTextFieldProperties {
-    value: string;
+export interface IUnderLinedTextFieldProperties {
+    value: string | undefined;
+    updateField: (_newValue: string | undefined) => void;
 }
 
-class UnderLinedTextField extends React.Component<IUnderLinedTextFieldProperties> {
-    
+export class UnderLinedTextField extends React.Component<IUnderLinedTextFieldProperties> {
+
     constructor(props: IUnderLinedTextFieldProperties) {
         super(props);
         //this.state = { value: props.value };
+        console.log("Props:", props);
+      
       };
 
       render() {
           return (
-            <TextField label="" defaultValue={this.props.value} underlined />
+            <TextField 
+              label="" 
+              onChange={(event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => { this.props.updateField(newValue); }} 
+              defaultValue={this.props.value} 
+              underlined 
+            />
           )
       }     
 }
+
 
 
 export default UnderLinedTextField;
